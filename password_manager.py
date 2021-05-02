06c909aa494b613414ea2d415a9e0f184eac6428
Vsevolod Vikhrov <uapbxxl@gmail.com>
2021-05-02 15:42:08 +0300

    release

--- password_manager.py
+++ password_manager.py
@@ -2,15 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, database_exists
 import argparse
 import base64
 import hashlib
 
 DB_FILE_NAME = 'passwords.db'
-
-engine = create_engine(f'sqlite:///{DB_FILE_NAME}', echo=True)
-
+engine = create_engine(f'sqlite:///{DB_FILE_NAME}')
 Base = declarative_base()
+
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 
 class Credentials(Base):
@@ -62,7 +64,8 @@ def parse_user_input():
 def set_passphrase(passphrase):
     backuped_passhprase = session.query(Passphrase).all()
     if not backuped_passhprase:
-        passphrase = hashlib.md5(b"passphrase").hexdigest()
+        passphrase = passphrase.encode()
+        passphrase = hashlib.md5(passphrase).hexdigest()
         session.add(Passphrase(passphrase))
         session.commit()
 
@@ -70,13 +73,16 @@ def set_passphrase(passphrase):
 def get_credentials(passphrase, site):
     backuped_passhprase = session.query(Passphrase).all()[0]
     backuped_passhprase = backuped_passhprase.passphrase
-    if hashlib.md5(b"passphrase").hexdigest() == backuped_passhprase:
+    passphrase = passphrase.encode()
+    if hashlib.md5(passphrase).hexdigest() == backuped_passhprase:
         credentials = session.query(Credentials).filter_by(site=site).first()
         site = credentials.site
         login = credentials.login
         password = credentials.password
         password = get_decoded_pass(password)
         print(f"Site: {site} Login: {login} Password: {password}")
+    else:
+        print("Incorrect passphrase!")
 
 
 def set_credentials(site, login, password):
@@ -86,13 +92,13 @@ def set_credentials(site, login, password):
 
 
 def get_encoded_pass(password):
-    password = base64.b64encode(b"password")
-    print(password)
+    password = password.encode()
+    password = base64.b64encode(password)
     return password
 
 
 def get_decoded_pass(password):
-    password = base64.b64decode(password).decode("utf-8")
+    password = base64.b64decode(password).decode()
     return password
 
 
--- test.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-
-a = "adw"
-a = hashlib.md5(b"a").hexdigest()
-
-print(a)
